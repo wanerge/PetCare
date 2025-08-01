594211c0230a136ed389cb2c99c776ff1489ba08
valpg182-wq <valpg182@gmail.com>
2025-08-01 18:00:16 -0500

    Update del update anterior

--- src/main/java/com/udea/petCare/service/CitasService.java
+++ src/main/java/com/udea/petCare/service/CitasService.java
@@ -2,9 +2,41 @@ package com.udea.petCare.service;
 
 import java.util.List;
 
-public interface CitasService {
-    List<CitasDTO> findAll();
-    CitasDTO save(CitasDTO alumnoDTO);
-    CitasDTO update(Integer id, CitasDTO alumnoDTO);
-    void delete(Integer id);
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udea.petCare.repository.CitasRepository;
+
+@Service
+public class CitasService {
+
+    @Autowired
+    private CitasRepository citasRepository;
+
+    @Override
+    public List<CitasDTO> findAll() {
+        List<Citas> all = (List<Citas>) citasRepository.findAll();
+        return all.stream().map(this::convertToDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public AlumnoDTO save(CitasDTO citasDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public CitasDTO update(Long id, CitasDTO citasDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete(Long id) {
+        citasRepository.deleteById(id);
+    }
+
+    private CitasDTO convertToDTO(Citas cita) {
+        return new CitasDTO(cita);
+    }
 }
