4135471abfadf031af422edd966e82db32fb656c
MFernandaQuinteroSoto <69827298+MFernandaQuinteroSoto@users.noreply.github.com>
2025-08-01 19:08:19 -0500

    adicion repositorio de Mascotas

---
+++ src/main/java/com/udea/petCare/dto/Mascotas.java
@@ -0,0 +1,18 @@
+package com.udea.petCare.dto;
+
+import java.security.Timestamp;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mascotas {
+    
+    private Long id_mascota;
+    private String nombre;
+    private Long id_especie;
+    private Timestamp fecha_nacimiento;
+    private Long id_genero;
+    private double peso;
+}
