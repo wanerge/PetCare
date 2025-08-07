cfee1077154187d18c27964a921e91ec562aed83
Uyoi22 <s.correa@udea.edu.co>
2025-08-07 18:16:10 -0500

    se añadio auth-interceptor para manejar los datos desde el login al client-dashboard

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,10 +3,15 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptorFn } from './app/core/login/interceptors/auth-interceptor';
+
 
 bootstrapApplication(App, {
-  providers: [provideRouter(routes),
-    provideHttpClient()
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([ authInterceptorFn ])
+    )
   ],
 });
