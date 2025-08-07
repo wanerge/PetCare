416f98eb1aebadd8891141c8b1f02930b186b792
leniaso <alejandro.naranjoc@udea.edu.co>
2025-08-07 17:00:36 -0500

    pruebas realizadas

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginCliente } from './core/login/login-cliente/login-cliente';
 import { LoginVeterinario } from './core/login/login-veterinario/login-veterinario';
-import { ClientDashboard } from './domains/clients/dashboard/client-dashboard/client-dashboard';
+import { MainClient } from './domains/clients/main-client/main-client';
 
 
 export const routes: Routes = [
   { path: 'login-cliente', component: LoginCliente },     
   { path: 'login-veterinario', component: LoginVeterinario },
-  { path: 'client-dashboard', component: ClientDashboard}
+  { path: 'MainClient', component: MainClient}
 
 ];
 
