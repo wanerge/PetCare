9eb612ed4f773b5d69a5916580c171af8034d96e
wanerge <wanerge100@hotmail.com>
2025-08-07 22:29:52 -0500

    Remove veterinarian dashboard route from app routing

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginCliente } from './core/login/login-cliente/login-cliente';
 import { LoginVeterinario } from './core/login/login-veterinario/login-veterinario';
 import { Home } from './layout/home/home';
-import { Main } from './domains/veterinarians/dashboard/main/main';
 import { ClientMain } from './domains/clients/dashboard/client-main/client-main';
 
 
@@ -12,7 +11,6 @@ export const routes: Routes = [
   { path: 'login-cliente', component: LoginCliente },
   { path: 'login-veterinario', component: LoginVeterinario },
   { path: 'client-main', component: ClientMain },
-  { path: 'vet-dashboard', component: Main },
   { path: '*', redirectTo: '' } // Redirige cualquier ruta desconocida a Home
 ];
 
